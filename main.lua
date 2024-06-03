bb1bd4c4c118588fd20d95bdc7da43b459564ce4
Aoki-x <72118930+Aoki-x@users.noreply.github.com>
2024-06-02 23:35:15 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local monsterName = "Creeper"
 local monsterDesc = "Um mob raivoso."
-local monsterAtk = 10
+local monsterHp = 10
+local monsterMaxHp = 10
+local monsterAtk = 8
 local monsterDef = 5
 local monsterSpe = 4
 local monsterInt = 6
@@ -23,19 +25,37 @@ local function getProguessBar(atribute)
     return result
 end
 
+local function createBar()
+    local bar = ""
+    for i = 1, 40, 1 do
+        bar = bar .. "#"
+    end
+    return bar
+end
+
 os.execute("chcp 65001")
 os.execute("cls")
 
-print("##################################")
+print(createBar())
 print("-> " .. monsterName)
 print("-->", monsterDesc)
 print("Som: " .. monsterSound)
 print("Horario de spawn: " .. monsterSpawnHour)
 print("Item dropavel: " .. monsterItem)
 print("--------------")
+print("Vida: ", getProguessBar(monsterHp))
 print("Ataque: ", getProguessBar(monsterAtk))
 print("Defesa: ", getProguessBar(monsterDef))
 print("Velocidade: ", getProguessBar(monsterSpe))
 print("Inteligencia: ", getProguessBar(monsterInt))
 print("--------------")
-print("##################################")
+print(createBar())
+
+while monsterHp > 0 do
+    print("Vida do " .. monsterName .. ": " .. monsterHp)
+    monsterHp = monsterHp - 1
+    if monsterHp == 5 then
+        print("Creeper explodiu!!!!")
+        monsterHp = 0
+    end
+end
